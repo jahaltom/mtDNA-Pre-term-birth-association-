9373fec4da142b77e76a3b632e2b8162fdcc6fee
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-09-12 13:56:46 -0400

    Update plink2VCF.sh

--- plink2VCF.sh
+++ plink2VCF.sh
@@ -7,8 +7,8 @@ bgzip clean.vcf
 bcftools index clean.vcf.gz
 
 #Extract mtDNA SNPs
-bcftools view --types snps  --regions 26 -S samplesC.txt clean.vcf.gz >  outmtDNA_C.vcf
-bcftools view --types snps  --regions 26 -S samplesM.txt clean.vcf.gz >  outmtDNA_M.vcf
+bcftools view --types snps  --regions 26  clean.vcf.gz >  outmtDNA.vcf
+
 
 #Makes ntDNA vcf for PCA.
 bcftools view --types snps -t ^26,25,23 -S samplesC.txt clean.vcf.gz   >  outntDNA_C.vcf
