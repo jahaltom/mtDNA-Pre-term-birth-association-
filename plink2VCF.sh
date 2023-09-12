bf5050e9b572b740d5c55fa1815ae1eea8a422a5
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2023-09-12 13:42:00 -0400

    Update plink2VCF.sh

--- plink2VCF.sh
+++ plink2VCF.sh
@@ -1,15 +1,16 @@
-#Take in plink files and make VCF
-plink --bfile momi5.clean --recode vcf --out yay
+#Take in plink files and makes VCF
+plink --bfile momi5.clean --recode vcf --out clean
 
-bgzip yay.vcf
+bgzip clean.vcf
 
 
-bcftools index yay.vcf.gz
+bcftools index clean.vcf.gz
 
 #Extract mtDNA SNPs
-bcftools view yay.vcf.gz --regions 26 | bcftools view --types snps | bcftools view -S samplesC.txt >  outmtDNA_C.vcf
-bcftools view yay.vcf.gz --regions 26 | bcftools view --types snps | bcftools view -S samplesM.txt >  outmtDNA_M.vcf
+bcftools view --types snps  --regions 26 -S samplesC.txt clean.vcf.gz >  outmtDNA_C.vcf
+bcftools view --types snps  --regions 26 -S samplesM.txt clean.vcf.gz >  outmtDNA_M.vcf
 
 #Makes ntDNA vcf for PCA.
-bcftools view --types snps -t ^26,25,23 yay.vcf.gz  | bcftools view -S samplesC.txt >  outntDNA_C.vcf
-bcftools view --types snps -t ^26,25,23 yay.vcf.gz  | bcftools view -S samplesM.txt >  outntDNA_M.vcf
+bcftools view --types snps -t ^26,25,23 -S samplesC.txt clean.vcf.gz   >  outntDNA_C.vcf
+bcftools view --types snps -t ^26,25,23 -S samplesM.txt clean.vcf.gz   >  outntDNA_M.vcf
+
