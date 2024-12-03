25592b42cef6a987f043d91f1a38af6495d12e56
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2024-12-03 09:33:55 -0500

    Update MetadataMerge.py

--- MetadataMerge.py
+++ MetadataMerge.py
@@ -1,86 +1,103 @@
 import pandas as pd
 import numpy as np
+ 
+        
 
-
-###Combine metadata and haplogroup data
+###########The subject ID in samples.tab  is BABY, PArticipant and ORIG_ID in momi_combined.data.tsv!!!!!!!!!!!!!!!!!!!!!!!!
 
 #Read in metadata
-md=pd.read_csv('momi5.pheno',sep='\t')  
-#Read in haplogroup data from Haplogrep3
-haplo=pd.read_csv('haplogrep3OUT',sep='\t')  
+md=pd.read_csv('samples.tab',sep='\t')  
+md = md.rename(columns={'id': 'SampleID'})
+
+haplo=pd.read_csv('haplogrep3OUT_22175',sep='\t')  
+haplo=pd.merge(md,haplo,on=["SampleID"])
+
+haplo=haplo.drop_duplicates(subset=['Subject_ID'])
 
 
-#Remove _C and _M  (Mother, Child) and repeat IDs from IDs
-haplo['ORIG_ID'] = haplo['SampleID'].str.split('_').str[0]
 
 
-#Merge
-md2=pd.merge(md,haplo,on=["ORIG_ID"])
+
+
+
+
+#Read in other metadata 
+md=pd.read_csv('momi_combined_data.txt',sep='\t')  
+md=md.drop_duplicates() 
+
+# ##Filter
+# md=md[(md["ALCOHOL"]!=4) ]
+# md=md[(md["SMOKE_HIST"]!=4) ]
+# md=md[(md["PASSIVE_SMOK"]!=1) ]
+
+# #Only live births
+md=md[md["PREG_OUTCOME"]==2]
+
+
+
+#Merge haplogroups with metadata
+mdPAR = md.rename(columns={'PARTICIPANT_ID': 'Subject_ID'})
+x=pd.merge(mdPAR,haplo,on=["Subject_ID"])
+
+
+mdBABY = md.rename(columns={'BABY_ID': 'Subject_ID'})
+x2=pd.merge(mdBABY,haplo,on=["Subject_ID"])
+
+mdORIG = md.rename(columns={'ORIG_ID': 'Subject_ID'})
+haplo['Subject_ID'] = haplo['Subject_ID'].str.replace('-M','')
+haplo['Subject_ID'] = haplo['Subject_ID'].str.replace('-C','')
+x3=pd.merge(mdORIG,haplo,on=["Subject_ID"])
+
+
+df=pd.concat([x,x2,x3])
+df=df.drop_duplicates(subset=['Subject_ID'])
+#Only high quality haplogroup calls. 
+df=df[df["Quality"]>=0.9]
+
+
 
 
 
 
 #Grab Main haplogroup
-md2["MainHap"] = md2["Haplogroup"].map(lambda s: next((hap for hap in ["L0","L1","L2","L3","L4","L5","HV"] if hap in s), "other"))
-md2['MainHap'] = np.where(md2['MainHap'] == 'other', md2['Haplogroup'].astype(str).str[0:1],md2["MainHap"])
+df["MainHap"] = df["Haplogroup"].map(lambda s: next((hap for hap in ["L0","L1","L2","L3","L4","L5","HV"] if hap in s), "other"))
+df['MainHap'] = np.where(df['MainHap'] == 'other', df['Haplogroup'].astype(str).str[0:1],df["MainHap"])
 
 #Grab sub haplogroups
 speHaps=["L0","L1","L2","L3","L4","L5","HV"]
-md2['SubHap'] = np.where(md2['MainHap'].isin(speHaps), md2['Haplogroup'].astype(str).str[0:3], md2['Haplogroup'].astype(str).str[0:2])
+df['SubHap'] = np.where(df['MainHap'].isin(speHaps), df['Haplogroup'].astype(str).str[0:3], df['Haplogroup'].astype(str).str[0:2])
+
+
+#Sep M and C 
+
+dfM=df[df["M/C"]=="M"]
+# Count occurrences of each category
+category_counts = dfM['MainHap'].value_counts()
+# Filter the DataFrame to include only categories with at least 10 occurrences 
+dfM = dfM[dfM['MainHap'].isin(category_counts[category_counts >= 10].index)]
+
+
+
+dfC=df[df["M/C"]=="C"]
+# Count occurrences of each category
+category_counts = dfC['MainHap'].value_counts()
+# Filter the DataFrame to include only categories with at least 10 occurrences
+dfC = dfC[dfC['MainHap'].isin(category_counts[category_counts >= 10].index)]
 
 
 
 
-md2=md2[['ORIG_ID', 'site_name', 'age', 'sex', 'ht', 'gday', 'ptb', 'bwt',
-       'twin', 'livebirth', 'SampleID', 'Haplogroup', 'MainHap', 'SubHap','Rank', 'Quality',
-       'Range', 'Not_Found_Polys', 'Found_Polys', 'Remaining_Polys',
-       'AAC_In_Remainings', 'Input_Sample']]
 
-
-    
-#Break md2 up by child and mother
-dfM = md2[md2['SampleID'].str.contains('_M')]
-dfC = md2[md2['SampleID'].str.contains('_C')]
-
-
-def makemetadata(d,string):
-    #Subset South Asian and African from data set   
-    afr=d[(d["site_name"]=="GAPPS-Zambia") | (d["site_name"]=="AMANHI-Tanzania") ].copy()
-    afr["Population"]="African"
-    sa =d[(d["site_name"]=="AMANHI-Bangladesh") | (d["site_name"]=="GAPPS-Bangladesh") | (d["site_name"]=="AMANHI-Pakistan") ].copy()
-    sa["Population"]="South_Asian"
-    
-    
-    def isAtLeast10(df):
-        #Make a df that marks a main haplogroup that has at least 10 samples.
-        x=(df["MainHap"].value_counts() >= 10).to_frame()
-        x= x.rename(columns={'MainHap': 'IsAtLeast10MainHap'})
-        x.index.name = "MainHap"
-        x.reset_index(inplace=True)
-        #Merge
-        df=pd.merge(df,x,on=["MainHap"])
-        #Make a df that marks a sub haplogroup that has at least 10 samples.
-        x=(df["SubHap"].value_counts() >= 10).to_frame()
-        x= x.rename(columns={'SubHap': 'IsAtLeast10SubHap'})
-        x.index.name = "SubHap"
-        x.reset_index(inplace=True)
-        #Merge
-        return pd.merge(df,x,on=["SubHap"])
-    
-    
-    
-    AFR=isAtLeast10(afr)
-    SA=isAtLeast10(sa)
-    
-    dfFinal=pd.concat([AFR,SA])
-    
-    dfFinal.to_csv("MetadataFinal."+string+".tsv", index=False, sep='\t')  
+dfC["Sample_ID"]=("0_"+dfC["SampleID"])
+dfC.to_csv("MetadataFinal.C.tsv", index=False, sep='\t')  
+pd.DataFrame("0_"+dfC["SampleID"]).to_csv("child.txt", index=False, sep='\t',header=False)  
 
 
 
-makemetadata(dfM, "M")
-makemetadata(dfC, "C") 
 
+dfM["Sample_ID"]=("0_"+dfM["SampleID"])
+dfM.to_csv("MetadataFinal.M.tsv", index=False, sep='\t')  
+pd.DataFrame("0_"+dfM["SampleID"]).to_csv("mom.txt", index=False, sep='\t',header=False)  
 
 
 
