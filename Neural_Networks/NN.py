fa54d95b2a46c62e98b7fe5b8d523670f1dd8dd5
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2024-12-10 15:54:40 -0500

    Update NN.py

--- Neural_Networks/NN.py
+++ Neural_Networks/NN.py
@@ -13,27 +13,34 @@ import keras_tuner as kt
 
 
 # Load metadata
-md = pd.read_csv("MetadataFinal.C.tsv",sep='\t')
+md = pd.read_csv("Metadata.M.Final.tsv",sep='\t')
 md.set_index('Sample_ID', inplace=True)
 md=md.astype(str)
+
+md=md[["PW_AGE",	"PW_EDUCATION",	"TYP_HOUSE",	"HH_ELECTRICITY",	"FUEL_FOR_COOK",	"DRINKING_SOURCE",	"TOILET",	"WEALTH_INDEX",	"SNIFF_TOBA",	"SNIFF_FREQ",	"SMOKE_HIST",	"SMOK_TYP",	"SMOK_FREQ",	"PASSIVE_SMOK",	"ALCOHOL",	
+    "ALCOHOL_FREQ",	"CHRON_HTN",	"DIABETES",	"TB",	"THYROID",	"EPILEPSY",	"MAT_HEIGHT",	"BIRTH_WEIGHT",	"GAGEBRTH_IN", 	"PTB",	"Sex",	"MainHap",	"PC1_All",	"PC2_All",	"PC3_All",	"PC4_All",	"PC5_All",	"PC6_All",	"PC7_All",	
+    "PC8_All",	"PC9_All",	"PC10_All",	"PC11_All",	"PC12_All",	"PC13_All",	"PC14_All",	"PC15_All",	"PC16_All",	"PC17_All",	"PC18_All",	"PC19_All",	"PC20_All",	"C1_All",	"C2_All",	"C3_All",	"C4_All",	"C5_All"]]
+
+
+
 
 
 #Catigorical 
 # One-hot encode haplogroups and gender
 data_encoded = pd.get_dummies(md, columns=['TYP_HOUSE', 'HH_ELECTRICITY', 'FUEL_FOR_COOK', 'DRINKING_SOURCE',
        'TOILET', 'WEALTH_INDEX', 'PASSIVE_SMOK', 'ALCOHOL', 'CHRON_HTN',
-       'DIABETES', 'TB', 'THYROID', 'EPILEPSY', 'FETAL_ANOMALIES', 'BABY_SEX',
-       'site', 'Sex', 'MainHap','SubHap'])
+       'DIABETES', 'TB', 'THYROID', 'EPILEPSY',
+        'Sex', 'MainHap',"SNIFF_TOBA","SMOKE_HIST"])
 
 print(data_encoded)
 
-cols=['TYP_HOUSE_-77', 'TYP_HOUSE_-88', 'TYP_HOUSE_1', 'TYP_HOUSE_2', 'HH_ELECTRICITY_-88', 'HH_ELECTRICITY_0', 'HH_ELECTRICITY_1', 'FUEL_FOR_COOK_-77', 'FUEL_FOR_COOK_-88', 'FUEL_FOR_COOK_1', 'FUEL_FOR_COOK_2', 'FUEL_FOR_COOK_3', 'FUEL_FOR_COOK_4', 'FUEL_FOR_COOK_5', 'DRINKING_SOURCE_-77', 'DRINKING_SOURCE_-88', 'DRINKING_SOURCE_1', 'DRINKING_SOURCE_2', 'DRINKING_SOURCE_3', 'DRINKING_SOURCE_4', 'TOILET_-88', 'TOILET_1', 'TOILET_2', 'TOILET_3', 'TOILET_4', 'WEALTH_INDEX_-77', 'WEALTH_INDEX_-88', 'WEALTH_INDEX_1', 'WEALTH_INDEX_2', 'WEALTH_INDEX_3', 'WEALTH_INDEX_4', 'WEALTH_INDEX_5', 'PASSIVE_SMOK_-77', 'PASSIVE_SMOK_0', 'PASSIVE_SMOK_1', 'ALCOHOL_-77', 'ALCOHOL_1', 'ALCOHOL_2', 'ALCOHOL_4', 'CHRON_HTN_-77', 'CHRON_HTN_0', 'CHRON_HTN_1', 'DIABETES_-77', 'DIABETES_0', 'DIABETES_1', 'TB_-77', 'TB_0', 'TB_1', 'THYROID_-77', 'THYROID_0', 'THYROID_1', 'EPILEPSY_-77', 'EPILEPSY_0', 'EPILEPSY_1', 'FETAL_ANOMALIES_-77', 'FETAL_ANOMALIES_-88', 'FETAL_ANOMALIES_0', 'BABY_SEX_1', 'BABY_SEX_2', 'site_AMANHI-Bangladesh', 'site_AMANHI-Pakistan', 'site_AMANHI-Pemba', 'site_GAPPS-Bangladesh', 'site_GAPPS-Zambia', 'Sex_1.0', 'Sex_2.0', 'Sex_nan', 'MainHap_A', 'MainHap_D', 'MainHap_E', 'MainHap_F', 'MainHap_G', 'MainHap_H', 'MainHap_HV', 'MainHap_J', 'MainHap_K', 'MainHap_L0', 'MainHap_L1', 'MainHap_L2', 'MainHap_L3', 'MainHap_L4', 'MainHap_M', 'MainHap_N', 'MainHap_R', 'MainHap_T', 'MainHap_U', 'MainHap_W', 'MainHap_X', 'MainHap_Z', 'SubHap_A+', 'SubHap_A1', 'SubHap_A2', 'SubHap_A6', 'SubHap_D4', 'SubHap_D5', 'SubHap_E1', 'SubHap_F1', 'SubHap_G1', 'SubHap_G2', 'SubHap_H1', 'SubHap_H2', 'SubHap_H4', 'SubHap_H5', 'SubHap_H6', 'SubHap_H7', 'SubHap_HV1', 'SubHap_HV2', 'SubHap_HV6', 'SubHap_J1', 'SubHap_J2', 'SubHap_K1', 'SubHap_K2', 'SubHap_L0a', 'SubHap_L0d', 'SubHap_L0f', 'SubHap_L1b', 'SubHap_L1c', 'SubHap_L2a', 'SubHap_L2b', 'SubHap_L2c', 'SubHap_L2d', 'SubHap_L3a', 'SubHap_L3b', 'SubHap_L3d', 'SubHap_L3e', 'SubHap_L3f', 'SubHap_L3h', 'SubHap_L3i', 'SubHap_L3k', 'SubHap_L3x', 'SubHap_L4b', 'SubHap_M1', 'SubHap_M2', 'SubHap_M3', 'SubHap_M4', 'SubHap_M5', 'SubHap_M6', 'SubHap_M7', 'SubHap_M8', 'SubHap_M9', 'SubHap_N1', 'SubHap_N2', 'SubHap_N5', 'SubHap_N8', 'SubHap_R', 'SubHap_R0', 'SubHap_R2', 'SubHap_R3', 'SubHap_R5', 'SubHap_R6', 'SubHap_R7', 'SubHap_R8', 'SubHap_T1', 'SubHap_T2', 'SubHap_U1', 'SubHap_U2', 'SubHap_U3', 'SubHap_U4', 'SubHap_U5', 'SubHap_U6', 'SubHap_U7', 'SubHap_U8', 'SubHap_U9', 'SubHap_W', 'SubHap_W3', 'SubHap_W4', 'SubHap_W6', 'SubHap_X2', 'SubHap_Z+', 'SubHap_Z3', 'SubHap_Z7']
+cols=[ 'TYP_HOUSE_-77.0', 'TYP_HOUSE_-88.0', 'TYP_HOUSE_1.0', 'TYP_HOUSE_2.0', 'HH_ELECTRICITY_-77.0', 'HH_ELECTRICITY_-88.0', 'HH_ELECTRICITY_0.0', 'HH_ELECTRICITY_1.0', 'FUEL_FOR_COOK_-77.0', 'FUEL_FOR_COOK_-88.0', 'FUEL_FOR_COOK_1.0', 'FUEL_FOR_COOK_2.0', 'FUEL_FOR_COOK_3.0', 'FUEL_FOR_COOK_4.0', 'FUEL_FOR_COOK_5.0', 'DRINKING_SOURCE_-77.0', 'DRINKING_SOURCE_-88.0', 'DRINKING_SOURCE_1.0', 'DRINKING_SOURCE_2.0', 'DRINKING_SOURCE_3.0', 'DRINKING_SOURCE_4.0', 'TOILET_-77.0', 'TOILET_-88.0', 'TOILET_1.0', 'TOILET_2.0', 'TOILET_3.0', 'TOILET_4.0', 'WEALTH_INDEX_-77.0', 'WEALTH_INDEX_-88.0', 'WEALTH_INDEX_1.0', 'WEALTH_INDEX_2.0', 'WEALTH_INDEX_3.0', 'WEALTH_INDEX_4.0', 'WEALTH_INDEX_5.0', 'PASSIVE_SMOK_-77.0', 'PASSIVE_SMOK_0.0', 'PASSIVE_SMOK_1.0', 'ALCOHOL_-77.0', 'ALCOHOL_1.0', 'ALCOHOL_2.0', 'ALCOHOL_4.0', 'CHRON_HTN_-77.0', 'CHRON_HTN_-88.0', 'CHRON_HTN_0.0', 'CHRON_HTN_1.0', 'DIABETES_-77.0', 'DIABETES_-88.0', 'DIABETES_0.0', 'DIABETES_1.0', 'TB_-77.0', 'TB_-88.0', 'TB_0.0', 'TB_1.0', 'THYROID_-77.0', 'THYROID_-88.0', 'THYROID_0.0', 'THYROID_1.0', 'EPILEPSY_-77.0', 'EPILEPSY_-88.0', 'EPILEPSY_0.0', 'EPILEPSY_1.0', 'Sex_2.0', 'Sex_nan', 'MainHap_A', 'MainHap_B', 'MainHap_C', 'MainHap_D', 'MainHap_E', 'MainHap_F', 'MainHap_G', 'MainHap_H', 'MainHap_HV', 'MainHap_I', 'MainHap_J', 'MainHap_K', 'MainHap_L0', 'MainHap_L1', 'MainHap_L2', 'MainHap_L3', 'MainHap_L4', 'MainHap_L5', 'MainHap_M', 'MainHap_N', 'MainHap_R', 'MainHap_T', 'MainHap_U', 'MainHap_W', 'MainHap_X', 'MainHap_Z', 'SNIFF_TOBA_-77.0', 'SNIFF_TOBA_-88.0', 'SNIFF_TOBA_1.0', 'SNIFF_TOBA_2.0', 'SNIFF_TOBA_3.0', 'SNIFF_TOBA_4.0', 'SMOKE_HIST_-77.0', 'SMOKE_HIST_-88.0', 'SMOKE_HIST_1.0', 'SMOKE_HIST_2.0', 'SMOKE_HIST_3.0', 'SMOKE_HIST_4.0']
 data_encoded[cols]=data_encoded[cols].astype(int)
 
 
 
 # Select continuous columns
-continuous_columns = ['PW_AGE', 'PW_EDUCATION', 'ROOMS_HOUSE', 'BIRTH_WEIGHT', 'MAT_HEIGHT','GAGEBRTH_IN']
+continuous_columns = ['PW_AGE','ALCOHOL_FREQ',"SNIFF_FREQ", "SMOK_FREQ",'PW_EDUCATION', 'BIRTH_WEIGHT', 'MAT_HEIGHT','GAGEBRTH_IN',"PC1_All",	"PC2_All",	"PC3_All",	"PC4_All",	"PC5_All",	"PC6_All",	"PC7_All",  "PC8_All",	"PC9_All",	"PC10_All",	"PC11_All",	"PC12_All",	"PC13_All",	"PC14_All",	"PC15_All",	"PC16_All",	"PC17_All",	"PC18_All",	"PC19_All",	"PC20_All",	"C1_All",	"C2_All",	"C3_All",	"C4_All",	"C5_All"]
 
 # Scale data to range [0, 1]
 scaler = MinMaxScaler()
@@ -42,9 +49,9 @@ data_encoded[continuous_columns] = scaler.fit_transform(data_encoded[continuous_
 print(data_encoded)
 
 
+data_encoded = data_encoded[data_encoded["PTB"] != "nan"]
 
-
-data_encoded[["PTB"]]=data_encoded[["PTB"]].astype(int)
+data_encoded[["PTB"]]= data_encoded[["PTB"]].astype(float).astype(int)
 
 
 
