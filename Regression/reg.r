729ab61c222d5b85258b4ca3e1fd02de16866169
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2024-12-10 14:27:47 -0500

    Update reg.r

--- Regression/reg.r
+++ Regression/reg.r
@@ -2,40 +2,41 @@ library(ISLR)
 library(ggplot2)
 library(dplyr)
 
-
-
 df=read.table("Metadata.M.Final.tsv",header=TRUE,sep = '\t',quote="")
-
-print("All Haplogroup breakdown")
-table(df$MainHap)
-
-print("African Haplogroup breakdown")
-table(df[df$population=="African",]$MainHap)
-
-print("South Asian Haplogroup breakdown")
-table(df[df$population=="SouthAsian",]$MainHap)
-
-
-
-
-print("Population")
+print(c("Population GA","Mother")
 glm.fit=glm(GAGEBRTH_IN~population , data=df  ) 
-print(summary (glm.fit ))
+summary (glm.fit )
+print(c("Population PTB(binary)","Mother"))
 glm.fit=glm(PTB~population , family="binomial", data=df  )
-print(summary (glm.fit ))
+summary (glm.fit )
 
-
-print("Site")
+print(c("Site GA","Mother")
 glm.fit=glm(GAGEBRTH_IN~site , data=df  ) 
-print(summary (glm.fit ))
+summary (glm.fit )
+print(c("Site PTB(binary)","Mother")
 glm.fit=glm(PTB~site , family="binomial", data=df  )
-print(summary (glm.fit ))
+summary (glm.fit )
+
         
+df=read.table("Metadata.C.Final.tsv",header=TRUE,sep = '\t',quote="")
+print(c("Population GA","Child")
+glm.fit=glm(GAGEBRTH_IN~population , data=df  ) 
+summary (glm.fit )
+print(c("Population PTB(binary)","Child"))
+glm.fit=glm(PTB~population , family="binomial", data=df  )
+summary (glm.fit )
+
+print(c("Site GA","Child")
+glm.fit=glm(GAGEBRTH_IN~site , data=df  ) 
+summary (glm.fit )
+print(c("Site PTB(binary)","Child")
+glm.fit=glm(PTB~site , family="binomial", data=df  )
+summary (glm.fit )
 
 
-model <- function(pop,ref,type) {
+model <- function(pop,ref,type,CoM) {
     
-    df=read.table("Metadata.M.Final.tsv",header=TRUE,sep = '\t',quote="")
+    df=read.table(paste("Metadata.",CoM,".Final.tsv",sep=""),header=TRUE,sep = '\t',quote="")
     
     if (type == 1) {
 
@@ -69,20 +70,20 @@ model <- function(pop,ref,type) {
     
     
     
-    print(c(pop,ref))
+    print(c(pop,ref,CoM))
     glm.fit=glm( pca , data=df  )
     print(summary (glm.fit ))
     
-    print(c(pop,ref))
+    print(c(pop,ref,CoM))
     glm.fit=glm( pcaBi , family="binomial", data=df  )
     print(summary (glm.fit ))
     
 
-    print(c(pop,ref))
+    print(c(pop,ref,CoM))
     glm.fit=glm( mds , data=df  )
     print(summary (glm.fit ))
     
-    print(c(pop,ref))
+    print(c(pop,ref,CoM))
     glm.fit=glm( mdsBi , family="binomial", data=df  )
     print(summary (glm.fit ))
     
@@ -91,10 +92,15 @@ model <- function(pop,ref,type) {
 
 
     
-model("All","M",1)   
-model("All","L3",1)  
-model("SouthAsian","M",2)  
-model("African","L3",2)  
+model("All","M",1,"C")   
+model("All","L3",1,"C")  
+model("SouthAsian","M",2,"C")  
+model("African","L3",2,"C")  
+    
+model("All","M",1,"M")   
+model("All","L3",1,"M")  
+model("SouthAsian","M",2,"M")  
+model("African","L3",2,"M")  
     
     
     
@@ -105,64 +111,44 @@ model("African","L3",2)
     
     
     
+    # popsSA=c("AMANHI-Bangladesh","AMANHI-Pakistan","GAPPS-Bangladesh")
+    # popsAf=c("AMANHI-Pemba","GAPPS-Zambia")
     
     
-    popsSA=c("AMANHI-Bangladesh","AMANHI-Pakistan","GAPPS-Bangladesh")
-    popsAf=c("AMANHI-Pemba","GAPPS-Zambia")
-    
-    
-    
-    
+
     
     
     
     
+    # #Haplogroup and ga
+    # png(paste("GAMainbHapJitterPTB.png",sep="_"),width=10,height=15,units="in",res=300)    
+    # print(ggplot(data=df, aes(x=MainHap, y=PTB)) +
+    # geom_violin(aes(fill=MainHap, color=MainHap)) +
+    # geom_crossbar(stat="summary", fun.y=mean, fun.ymax=mean, fun.ymin=mean, fatten=2, width=.5) +
+    # geom_point(color="black", size=1, position = position_jitter(w=0.05)) +
+    # theme_minimal())    
+    # dev.off()  
     
     
     
+    # png(paste("GASubbHapJitterPTB.png",sep="_"),width=30,height=15,units="in",res=300)    
+    # print(ggplot(data=df, aes(x=SubHap, y=PTB)) +
+    # geom_violin(aes(fill=SubHap, color=SubHap)) +
+    # geom_crossbar(stat="summary", fun.y=mean, fun.ymax=mean, fun.ymin=mean, fatten=2, width=.5) +
+    # geom_point(color="black", size=1, position = position_jitter(w=0.05)) +
+    # theme_minimal())    
+    # dev.off()   
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #Haplogroup and ga
-    png(paste("GAMainbHapJitterPTB.png",sep="_"),width=10,height=15,units="in",res=300)    
-    print(ggplot(data=df, aes(x=MainHap, y=PTB)) +
-    geom_violin(aes(fill=MainHap, color=MainHap)) +
-    geom_crossbar(stat="summary", fun.y=mean, fun.ymax=mean, fun.ymin=mean, fatten=2, width=.5) +
-    geom_point(color="black", size=1, position = position_jitter(w=0.05)) +
-    theme_minimal())    
-    dev.off()  
-    
-    
-    
-    png(paste("GASubbHapJitterPTB.png",sep="_"),width=30,height=15,units="in",res=300)    
-    print(ggplot(data=df, aes(x=SubHap, y=PTB)) +
-    geom_violin(aes(fill=SubHap, color=SubHap)) +
-    geom_crossbar(stat="summary", fun.y=mean, fun.ymax=mean, fun.ymin=mean, fatten=2, width=.5) +
-    geom_point(color="black", size=1, position = position_jitter(w=0.05)) +
-    theme_minimal())    
-    dev.off()   
-    
-    
-    png(paste("GAMainbHapJitterWealthIndex.png",sep="_"),width=10,height=15,units="in",res=300)
-    print(ggplot(data=df, aes(x=WEALTH_INDEX, y=PTB)) +
-    geom_violin(aes(fill=WEALTH_INDEX, color=WEALTH_INDEX)) +
-    geom_crossbar(stat="summary", fun.y=mean, fun.ymax=mean, fun.ymin=mean, fatten=2, width=.5) +
-    geom_point(color="black", size=1, position = position_jitter(w=0.05)) +
-    theme_minimal())    
-    dev.off()   
+    # png(paste("GAMainbHapJitterWealthIndex.png",sep="_"),width=10,height=15,units="in",res=300)
+    # print(ggplot(data=df, aes(x=WEALTH_INDEX, y=PTB)) +
+    # geom_violin(aes(fill=WEALTH_INDEX, color=WEALTH_INDEX)) +
+    # geom_crossbar(stat="summary", fun.y=mean, fun.ymax=mean, fun.ymin=mean, fatten=2, width=.5) +
+    # geom_point(color="black", size=1, position = position_jitter(w=0.05)) +
+    # theme_minimal())    
+    # dev.off()   
 
   
-}
               
 
 
