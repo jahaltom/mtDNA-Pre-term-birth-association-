579bb16b8ff8bc0f0538f9bae216566fc57ac66c
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2024-12-16 15:02:10 -0500

    Update HeteroplasmyTXTprocessing.py

--- Heteroplasmy Neural Networks/HeteroplasmyTXTprocessing.py
+++ Heteroplasmy Neural Networks/HeteroplasmyTXTprocessing.py
@@ -28,24 +28,11 @@ for d in dataframes[1:]:
 merged_df['POS'] = merged_df['Var'].str.extract('(\d+)').astype(int)
 df_sorted = merged_df.sort_values('POS')
 
-
-
 df_sorted = df_sorted.fillna(0) 
-  
-
- 
 df_sorted.to_csv('data.csv', index=None)
  
 
- 
-
 #df_sorted.iloc[:, 1:df_sorted.shape[1]-1] = df_sorted.iloc[:, 1:df_sorted.shape[1]-1].apply(pd.to_numeric, errors='coerce')
-
-
-
-
-
- 
 
 
 cOm=["C","M"]
@@ -62,24 +49,15 @@ for f in cOm:
     per10=df_filt[df_filt.columns[1:df_filt.shape[1]-1]].shape[1]*.10
     # Condition: Remove rows where less than 10% columns are > 0  
     df_filt=df_filt[(df_filt[df_filt.columns[1:df_filt.shape[1]-1]] > 0).sum(axis=1) >= per10] 
-
-
-
-
+    
     # Step 1: Reshape df_freq into long format
     df_long = pd.melt(df_filt, id_vars=['Var', 'POS'], 
                   value_vars=df_filt.columns[1:df_filt.shape[1]-1],
                   var_name='SampleID', value_name='Frequency')
-
-
+    
     # Step 2: Merge with race metadata
     df_merged = df_long.merge(dfCM, on='SampleID')
-    
-    
-    
-    
-    
-    
+
     ##########################################Population
     # Step 3: Map colors to races
     race_to_color = {'SouthAsian': 'blue', 'African': 'green'}  # Define color mapping
@@ -131,41 +109,26 @@ for f in cOm:
     plt.savefig("Heteroplasmy."+f+".PTB.png", bbox_inches="tight")
     plt.clf()   
     
-    
-    
-    
-    
+
     ####NN
-    
-    
-    
+
     dfCM=dfCM[["SampleID"]]
-    
-    
     #Drop the 'POS' column if it's no longer needed
     df = df_filt.drop(columns='POS')
     df = df.set_index('Var')
     df=df.T
     # Convert rownames (index) to a column
     df = df.reset_index()
-    
     # Rename the new column if needed (optional)
     df.rename(columns={'index': 'SampleID'}, inplace=True)
-    
     df=pd.merge(dfCM,df,on=["SampleID"])     
-    
-    
-    
-    
+
     continuous_columns=df.drop(["SampleID"], axis=1).columns.to_list()
     
     # Assuming `vaf_matrix` is your matrix of variant allele frequencies
     scaler = MinMaxScaler()
     df[continuous_columns] = scaler.fit_transform(df[continuous_columns])
-    
-    
-    
-    
+
     df.to_csv('HetroplasmyNN.'+f+'.tsv', index=None,sep='\t')
 
 
