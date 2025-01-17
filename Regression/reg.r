d2b16da79e9d538e4ac018b8b0810ff75669c0ef
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-01-17 09:22:16 -0500

    Update reg.r

--- Regression/reg.r
+++ Regression/reg.r
@@ -20,21 +20,30 @@ stats <- function(model) {
 
 
 
-
-
-
         
 CoM="M"
 
 
 df=read.table(paste("Metadata.",CoM,".Final.tsv",sep=""),header=TRUE,sep = '\t',quote="")
 
-print("What it is")
-table(df$MainHap)
+print(paste("What it is" , CoM,sep=""))
+
 
 
 df$PTB <- factor(df$PTB)
 print(c("PTB levels ",levels(df$PTB)))
+
+
+
+table(df$MainHap)
+df <- df %>%
+        group_by(MainHap) %>%
+        filter(n() >= 10) %>%
+        ungroup()
+
+table(df$MainHap)
+
+
 
 
 # Initialize Population column if it does not exist
@@ -43,22 +52,24 @@ if (!"Population" %in% colnames(df)) {
 }
 
 # Assign "African" to Population if site is "AMANHI-Pemba" or "GAPPS-Zambia"
-df$Population <- ifelse(df$site %in% c("AMANHI-Pemba", "GAPPS-Zambia"), "African", "South Asian")
-
-
-
-print(c("Population GA","Child"))
+df$Population <- ifelse(df$site %in% c("AMANHI-Pemba", "GAPPS-Zambia"), "African", "South Asian"
+                        
+                        
+table(df$Population)
+print(c("Population - GA Dataset: ",CoM))
 glm.fit=glm(GAGEBRTH~Population , data=df  ) 
 summary (glm.fit )
-print(c("Population PTB(binary)","Child"))
-glm.fit=glm(PTB~population , family="binomial", data=df  )
+print(c("Population - PTB Dataset: ",CoM))
+glm.fit=glm(PTB~Population , family="binomial", data=df  )
 summary (glm.fit )
 stats(glm.fit)
 
-print(c("Site GA","Child"))
+table(df$site)
+df$site= relevel(factor(df$site), ref="AMANHI-Pemba")
+print(c("Site - GA Dataset: ",CoM))
 glm.fit=glm(GAGEBRTH~site , data=df  ) 
 summary (glm.fit )
-print(c("Site PTB(binary)","Child"))
+print(c("Site - PTB Dataset: )",CoM))
 glm.fit=glm(PTB~site , family="binomial", data=df  )
 summary (glm.fit )
 stats(glm.fit)
@@ -66,52 +77,73 @@ stats(glm.fit)
 
 
 
-
-
-
-  
-
-
-        
-
-    #Set reference haplogroups
-    df$MainHap= relevel(factor(df$MainHap), ref="M")
-    
-    
-    
-
-    glm.fit=glm( GAGEBRTH ~ MainHap + PC1 + PC2 + PC3 + PC4 + PC5  , data=df  )
-    print(summary (glm.fit ))
-    
-
-    glm.fit=glm( PTB ~ MainHap + PC1 + PC2 + PC3 + PC4 + PC5  , family="binomial", data=df  )
-    print(summary (glm.fit ))
-    stats(glm.fit)
+ref="M" 
+#Set reference haplogroups
+df$MainHap= relevel(factor(df$MainHap), ref=ref)
+print(paste("Hapologroup and PCA - GA, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( GAGEBRTH ~ MainHap + PC1 + PC2 + PC3 + PC4 + PC5  , data=df  )
+print(summary (glm.fit ))
+print(paste("Hapologroup and PCA - PTB, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( PTB ~ MainHap + PC1 + PC2 + PC3 + PC4 + PC5  , family="binomial", data=df  )
+print(summary (glm.fit ))
+stats(glm.fit)
+print(paste("Hapologroup and MDS - GA, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( GAGEBRTH ~ MainHap + C1 + C2 + C3 + C4 + C5  , data=df  )
+print(summary (glm.fit ))
+print(paste("Hapologroup and MDS - PTB, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( PTB ~ MainHap + C1 + C2 + C3 + C4 + C5  , family="binomial", data=df  )
+print(summary (glm.fit ))
+stats(glm.fit)
    
 
-
-    glm.fit=glm( GAGEBRTH ~ MainHap + C1 + C2 + C3 + C4 + C5  , data=df  )
-    print(summary (glm.fit ))
+ref="HV" 
+#Set reference haplogroups
+df$MainHap= relevel(factor(df$MainHap), ref=ref)
+print(paste("Hapologroup and PCA - GA, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( GAGEBRTH ~ MainHap + PC1 + PC2 + PC3 + PC4 + PC5  , data=df  )
+print(summary (glm.fit ))
+print(paste("Hapologroup and PCA - PTB, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( PTB ~ MainHap + PC1 + PC2 + PC3 + PC4 + PC5  , family="binomial", data=df  )
+print(summary (glm.fit ))
+stats(glm.fit)
+print(paste("Hapologroup and MDS - GA, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( GAGEBRTH ~ MainHap + C1 + C2 + C3 + C4 + C5  , data=df  )
+print(summary (glm.fit ))
+print(paste("Hapologroup and MDS - PTB, Ref=", ref ," Dataset: ",CoM,sep=""))
+glm.fit=glm( PTB ~ MainHap + C1 + C2 + C3 + C4 + C5  , family="binomial", data=df  )
+print(summary (glm.fit ))
+stats(glm.fit)
+    
+    
+    
+    
     
 
-    glm.fit=glm( PTB ~ MainHap + C1 + C2 + C3 + C4 + C5  , family="binomial", data=df  )
-    print(summary (glm.fit ))
-    stats(glm.fit)
-   
 
 
-    
-    
-    
-    
-    
-    
-    
+# table(df$SubHap)
+# df <- df %>%
+#         group_by(SubHap) %>%
+#         filter(n() >= 20) %>%
+#         ungroup()
 
+# table(df$SubHap)
 
-
-
-
-
-
+# ref="M3" 
+# #Set reference haplogroups
+# df$SubHap= relevel(factor(df$SubHap), ref=ref)
+# print(paste("Hapologroup and PCA - GA, Ref=", ref ," Dataset: ",CoM,sep=""))
+# glm.fit=glm( GAGEBRTH ~ SubHap + PC1 + PC2 + PC3 + PC4 + PC5  , data=df  )
+# print(summary (glm.fit ))
+# print(paste("Hapologroup and PCA - PTB, Ref=", ref ," Dataset: ",CoM,sep=""))
+# glm.fit=glm( PTB ~ SubHap + PC1 + PC2 + PC3 + PC4 + PC5  , family="binomial", data=df  )
+# print(summary (glm.fit ))
+# stats(glm.fit)
+# print(paste("Hapologroup and MDS - GA, Ref=", ref ," Dataset: ",CoM,sep=""))
+# glm.fit=glm( GAGEBRTH ~ SubHap + C1 + C2 + C3 + C4 + C5  , data=df  )
+# print(summary (glm.fit ))
+# print(paste("Hapologroup and MDS - PTB, Ref=", ref ," Dataset: ",CoM,sep=""))
+# glm.fit=glm( PTB ~ SubHap + C1 + C2 + C3 + C4 + C5  , family="binomial", data=df  )
+# print(summary (glm.fit ))
+# stats(glm.fit)
 
