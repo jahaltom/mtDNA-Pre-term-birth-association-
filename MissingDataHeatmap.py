a4cdeede0932cc7d8c8922126f7f2cd795f41df8
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-02-21 15:12:11 -0500

    Update MissingDataHeatmap.py

--- MissingDataHeatmap.py
+++ MissingDataHeatmap.py
@@ -24,13 +24,12 @@ site_colors = {site: site_palette[i] for i, site in enumerate(unique_sites)}
 row_colors = row_labels.map(site_colors)
 
 # Select relevant columns for the heatmap
-df = df[['PW_AGE', 'SNIFF_FREQ', 'PW_EDUCATION', 'MAT_HEIGHT', 'MAT_WEIGHT',
+df = df[['PW_AGE', 'PW_EDUCATION', 'MAT_HEIGHT', 'MAT_WEIGHT',
                    'TYP_HOUSE', 'HH_ELECTRICITY', 'FUEL_FOR_COOK', 'DRINKING_SOURCE',
-                       'TOILET', 'WEALTH_INDEX', 'PASSIVE_SMOK', 'ALCOHOL', 'CHRON_HTN',
+                       'TOILET', 'WEALTH_INDEX', 'PASSIVE_SMOK','CHRON_HTN',
                        'DIABETES', 'TB', 'THYROID', 'EPILEPSY', 'BABY_SEX', 'MainHap',
-                       'SNIFF_TOBA', 'SMOKE_HIST']]
-
-
+                       'ALCOHOL' , 'ALCOHOL_FREQ' ,'SMOKE_HIST','SMOK_FREQ', 'SMOK_TYP','SMOK_YR','SNIFF_TOBA','SNIFF_FREQ']]
+                      
 
 
 
