7612eaf32d2be2d87c916df30a73afa89d2dc4df
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-03-07 11:08:01 -0500

    Update CombinePCA-MDS.py

--- CombinePCA-MDS.py
+++ CombinePCA-MDS.py
@@ -3,17 +3,18 @@ from functools import reduce
 
 
 
-sets=["M","C"]
-for s in sets:   
-    header=["FID","IID"]+[f"PC{i}" for i in range(1, 21)]
-    pca = pd.read_csv("PCA-MDS/"+s+".eigenvec",sep='\s+',header=None)
-    pca.columns=header
-    pca = pca.drop(columns=['IID'])
-    mds = pd.read_csv("PCA-MDS/"+s+".mds",sep='\s+')
-    mds = mds.drop(columns=['IID','SOL'])
-    df=pd.merge(pca,mds,on=["FID"]) 
-    df = df.rename(columns={"FID":"Sample_ID"})
-    md = pd.read_csv("Metadata."+s+".Weibull.tsv",sep='\t')  
-    dfFinal=pd.merge(md,df,on=["Sample_ID"])     
-    dfFinal.to_csv("Metadata."+s+".Final.tsv", index=False,sep="\t") 
+ 
+
+
+header=["FID","IID"]+[f"PC{i}" for i in range(1, 21)]
+pca = pd.read_csv("PCA-MDS/out.eigenvec",sep='\s+',header=None)
+pca.columns=header
+pca = pca.drop(columns=['IID'])
+mds = pd.read_csv("PCA-MDS/out.mds",sep='\s+')
+mds = mds.drop(columns=['IID','SOL'])
+df=pd.merge(pca,mds,on=["FID"]) 
+df = df.rename(columns={"FID":"Sample_ID"})
+md = pd.read_csv("Metadata.Weibull.tsv",sep='\t')  
+dfFinal=pd.merge(md,df,on=["Sample_ID"])     
+dfFinal.to_csv("Metadata.Final.tsv", index=False,sep="\t") 
 
