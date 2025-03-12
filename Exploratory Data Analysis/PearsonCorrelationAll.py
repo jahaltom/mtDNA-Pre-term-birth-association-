2d0f5b3a2f70dddae0b4360a3a0fd9b8fabae13d
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-03-12 13:22:00 -0400

    Update PearsonCorrelationAll.py

--- Exploratory Data Analysis/PearsonCorrelationAll.py
+++ Exploratory Data Analysis/PearsonCorrelationAll.py
@@ -15,7 +15,7 @@ continuous_columns=sys.argv[2].split(',') + ["PC1", "PC2", "PC3", "PC4", "PC5"]
 
 
 # Mixed Feature Correlation: One-Hot Encode categorical features
-encoded_df = pd.get_dummies(df[categorical_columns], drop_first=True)
+encoded_df = pd.get_dummies(df[categorical_columns], drop_first=False)
 encoded_df = encoded_df.astype(int)
 mixed_df = pd.concat([df[continuous_columns + ['GAGEBRTH','PTB']], encoded_df], axis=1)
 
@@ -28,5 +28,5 @@ sns.heatmap(corr_matrix, annot=False, cmap='coolwarm', center=0)
 plt.title("Correlation Matrix for All Variables")
 plt.tight_layout()
 plt.show()
-plt.savefig("PCACorr.png")
+plt.savefig("CorrAll.png")
 plt.close()
