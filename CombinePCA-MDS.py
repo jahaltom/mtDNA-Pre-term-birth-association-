cf796fcda68cee86ff864bce48a624885596fa08
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-03-14 09:32:37 -0400

    Update CombinePCA-MDS.py

--- CombinePCA-MDS.py
+++ CombinePCA-MDS.py
@@ -37,20 +37,23 @@ pc_percentage = (eigenvalues / total_variance) * 100
 features = ["MainHap", "SubHap", "site"]
 
 for f in features:
+    # Map categorical data to colors
+    unique_categories = md[f].unique()
+    color_map = {category: i for i, category in enumerate(unique_categories)}
+    colors = md[f].map(color_map)
     # Create PC1 vs PC2 scatter plot with color by feature
     plt.figure()
-    plt.scatter(md['PC1'], md['PC2'], c=md[f], cmap='viridis', alpha=0.8, s=1)
-    plt.colorbar(label=f)
+    scatter = plt.scatter(md['PC1'], md['PC2'], c=colors, cmap='viridis', alpha=0.8, s=1)
+    plt.colorbar(scatter, label=f)
     plt.title("PC1 vs PC2")
     plt.xlabel(f"PC1 ({round(pc_percentage.iloc[0], 1)}% variance)")
     plt.ylabel(f"PC2 ({round(pc_percentage.iloc[1], 1)}% variance)")
     plt.savefig(f"{f}.PCA.png")
     plt.close()
-
     # Create C1 vs C2 scatter plot with color by feature
     plt.figure()
-    plt.scatter(md['C1'], md['C2'], c=md[f], cmap='viridis', alpha=0.8, s=1)
-    plt.colorbar(label=f)
+    scatter = plt.scatter(md['C1'], md['C2'], c=colors, cmap='viridis', alpha=0.8, s=1)
+    plt.colorbar(scatter, label=f)
     plt.title("C1 vs C2")
     plt.xlabel("C1")
     plt.ylabel("C2")
