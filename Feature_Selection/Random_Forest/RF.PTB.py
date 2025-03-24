e4af15f206ddd68eae8e98f671869a0cf25acaaa
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-03-24 19:56:28 -0400

    Update RF.PTB.py

--- Feature_Selection/Random_Forest/RF.PTB.py
+++ Feature_Selection/Random_Forest/RF.PTB.py
@@ -220,6 +220,7 @@ PartialDependenceDisplay.from_estimator(
     feature_names=feature_names,
     grid_resolution=20,
     ax=ax
+    percentiles=(0.001, 0.999)  # Adjust these values if necessary
 )
 plt.savefig("PDP_Top5.RF.PTB.png", bbox_inches="tight")
 plt.clf()
