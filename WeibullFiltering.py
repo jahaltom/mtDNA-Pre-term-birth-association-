aa0c6aa21ae894a03dd29be599ef9ee9a5bdc8dc
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-09-22 13:37:17 -0400

    Update WeibullFiltering.py

--- WeibullFiltering.py
+++ WeibullFiltering.py
@@ -6,22 +6,16 @@ import sys
 import os
 import seaborn as sns
 
-# Load metadata
-md = pd.read_csv(sys.argv[1],sep='\t')
-md=md.dropna(subset=["GAGEBRTH","PTB"])
 
 
-catigoricalFeat= [item for item in sys.argv[2].split(',') if item != ''] 
-contFeat= [item for item in sys.argv[3].split(',') if item != ''] 
+
+catigoricalFeat= [item for item in sys.argv[1].split(',') if item != '']  #
+contFeat= [item for item in sys.argv[2].split(',') if item != '']  #
 
 
 
 
-#All columns we want analized
-wantedCol = catigoricalFeat + contFeat
-
-# Apply filtering only in the wantedCol columns. Remove missing data rows. 
-md = md[~md[wantedCol].isin([-88, -77,-99]).any(axis=1)]
+md =pd.read_csv("MetadataOutlierRemoved.tsv",sep='\t')
 
 
 
