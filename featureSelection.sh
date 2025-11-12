06c21a5717cb95d508420bdaf00e5e5a442b37a2
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-11-12 16:45:38 -0500

    Update featureSelection.sh

--- featureSelection.sh
+++ featureSelection.sh
@@ -30,8 +30,8 @@ function run_analysis {
 # Testing different configurations
 run_analysis "GB.GA.py" "Feature_Selection/Gradient_Boosting/GA"
 run_analysis "GB.PTB.py" "Feature_Selection/Gradient_Boosting/PTB"
-run_analysis "NN.GA.py" "Feature_Selection/NeuralNetworks"
-run_analysis "NN.PTB.py" "Feature_Selection/NeuralNetworks"
+run_analysis "NN.GA.py" "Feature_Selection/NeuralNetworks/GA"
+run_analysis "NN.PTB.py" "Feature_Selection/NeuralNetworks/PTB"
 run_analysis "RF.GA.py" "Feature_Selection/Random_Forest/GA"
 run_analysis "RF.PTB.py" "Feature_Selection/Random_Forest/PTB"
 run_analysis "LinearRegGA.py" "Feature_Selection/Linear-Logistic_Regression"
