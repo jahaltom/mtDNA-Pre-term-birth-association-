7d2a15ee0d0a95918602215fecdfc13279210273
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2025-12-04 16:17:05 -0500

    Update finalModel.sh

--- finalModel.sh
+++ finalModel.sh
@@ -8,65 +8,95 @@
 source /home/haltomj/miniconda3/etc/profile.d/conda.sh
 
 
-
 conda activate ML
+
 
 #Input file
 file="Metadata.M.tsv"
+
 # Define Categorical features
 columnsCat=('MainHap')
 # Define Continuous  features
-columnsCont=('BMI','PW_AGE')
-
-
-
-
-
+columnsCont=CONT
 
 # Convert the array to a comma-separated string
-
 columnCat_string=$( echo "${columnsCat[*]}")
 columnCont_string=$( echo "${columnsCont[*]}")
 
 
 
-cp "$file" Final_Model/
-cp scripts/WeibullFiltering.py Final_Model/
-cp scripts/CombinePCA.py Final_Model/
-cp scripts/outlierPCA.py Final_Model/
-cd Final_Model/
-
-# Call the Python script with the column string as an argument
-python WeibullFiltering.py $file "$columnCat_string" "$columnCont_string" 
 
 
 
+cp $file Final_Model
+cd Final_Model
 
-bcftools view --types snps -t ^26,24,23 -S IDs.txt --force-samples /scr1/users/haltomj/PTB/plink2.vcf   >  plink2.vcf
+#Removes samples where gestational age "GAGEBRTH" or  PTB (0 or 1) is na. Also removes samples with missing data in any of the input columns.
+python ../scripts/removeMissingData.py $file "$columnCat_string" "$columnCont_string"
+
+
+
+
 
 conda activate plink
+
+
+
+plink \
+  --bfile /scr1/users/haltomj/PTB/plink2 \
+  --keep IDs.txt \
+  --chr 1-22 \
+  --snps-only just-acgt \
+  --biallelic-only strict \
+  --geno 0.05 \
+  --mind 0.05 \
+  --maf 0.01 \
+  --hwe 1e-6 midp \
+  --threads 8 \
+  --make-bed --out nDNA_final > qc.log 2>&1
+
+
 
 mkdir PCA
-#Run plink PCA
-plink --vcf plink2.vcf --pca --double-id --out PCA/out
+# PCA for ancestry covariates
+plink --bfile nDNA_final --pca 10 --out PCA/out
+
+# QC summary stats
+plink --bfile nDNA_final --missing --freq --out nDNA_stats
+
+
+
 
 conda activate ML
-python outlierPCA.py
+python ../scripts/outlierPCA.py
+
+
+# Call the Python script with the column string as an argument
+python ../scripts/WeibullFiltering.py "$columnCat_string" "$columnCont_string" > out.txt
+
+
+
+
+
+
 
 conda activate plink
-# Recompute PCA using filtered samples
-plink --vcf plink2.vcf --keep keep_samples.txt --pca --double-id --out PCA/cleaned
+
+
+
+plink --bfile nDNA_final --keep IDs2.txt --make-bed --out nDNA_final2
+
+mkdir PCA2
+#Run plink PCA
+plink --bfile nDNA_final2 --pca 10 --out PCA2/cleaned
+
 
 
 
 conda activate ML
-python  CombinePCA.py
+python  ../scripts/CombinePCA.py
 
 
 
-
-
-#Final model 
-python finalModel.py "$columnCat_string" "$columnCont_string" > detailedModelSummary.txt
 
 
