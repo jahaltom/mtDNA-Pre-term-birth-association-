bb69b0b9f7724f85c47dfb475765c2e99af37ac2
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2026-04-17 11:58:24 -0400

    Fix categorical_columns assignment to avoid nesting

--- Feature_Selection/NeuralNetworks/GA/NN.GA.py
+++ Feature_Selection/NeuralNetworks/GA/NN.GA.py
@@ -26,7 +26,7 @@ tf.random.set_seed(SEED)
 df = pd.read_csv("Metadata.Final.tsv", sep='\t')
 
 # Define features
-categorical_columns = [sys.argv[1].split(',')]
+categorical_columns = sys.argv[1].split(',')
 continuous_columns = sys.argv[2].split(',')
 if len(sys.argv) > 3 and sys.argv[3].strip():
     binary_columns = sys.argv[3].split(',')
