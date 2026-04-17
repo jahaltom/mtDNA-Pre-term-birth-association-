9aee70800b4719408b3c75b3eb89582fcb83cd1b
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2026-04-17 11:49:46 -0400

    Fix categorical_columns assignment to use nested list

--- Feature_Selection/Gradient_Boosting/PTB/GB.PTB.py
+++ Feature_Selection/Gradient_Boosting/PTB/GB.PTB.py
@@ -24,7 +24,7 @@ from sklearn.pipeline import make_pipeline
 
 df = pd.read_csv("Metadata.Final.tsv", sep="\t")
 
-categorical_columns = [c for c in sys.argv[1].split(',') if c != "site"]
+categorical_columns = [sys.argv[1].split(',')]
 continuous_columns = sys.argv[2].split(",")
 if len(sys.argv) > 3 and sys.argv[3].strip():
     binary_columns = sys.argv[3].split(',')
