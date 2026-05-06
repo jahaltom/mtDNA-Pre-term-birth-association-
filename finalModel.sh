e1bc5869122b0945c9804cfe142ff29c7cb0909d
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2026-05-05 22:29:57 -0400

    Add site and ref variables to finalModel.sh

--- finalModel.sh
+++ finalModel.sh
@@ -19,7 +19,8 @@ columnsCat=CAT
 # Define Continuous  features
 columnsCont=CONT
 target=TARGET
-
+site=SITE
+ref=REF
 
 # Convert the array to a comma-separated string
 columnCat_string=$( echo "${columnsCat[*]}")
@@ -102,5 +103,7 @@ python  ../scripts/CombinePCA.py
 
 module load R
 sed -i "s/MainHap/$target/g" finalModel_freq_vs_bayesian.r
+sed -i "s/REF/$ref/g" finalModel_freq_vs_bayesian.r
+sed -i "s/SITE/$site/g" finalModel_freq_vs_bayesian.r
 Rscript finalModel_freq_vs_bayesian.r
 
