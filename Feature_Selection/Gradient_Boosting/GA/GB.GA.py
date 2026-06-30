2ae3318b79261741324237bff27d3874529e0eee
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2026-06-30 14:05:06 -0400

    Refactor feature argument parsing logic
    Refactor feature parsing into a separate function for better readability and maintainability.

--- Feature_Selection/Gradient_Boosting/GA/GB.GA.py
+++ Feature_Selection/Gradient_Boosting/GA/GB.GA.py
@@ -16,14 +16,14 @@ from sklearn.model_selection import GroupKFold
 
 
 
-# Define features
-categorical_columns = sys.argv[1].split(',')
-continuous_columns = sys.argv[2].split(',')
-if len(sys.argv) > 3 and sys.argv[3].strip():
-    binary_columns = sys.argv[3].split(',')
-else:
-    binary_columns = []   # <- default when no 3rd arg
+def parse_arg(i):
+    if len(sys.argv) <= i or not sys.argv[i].strip():
+        return []
+    return [x.strip() for x in sys.argv[i].split(",") if x.strip()]
 
+categorical_columns = parse_arg(1)
+continuous_columns = parse_arg(2)
+binary_columns = parse_arg(3)
 
 
 df = pd.read_csv("Metadata.Final.tsv", sep="\t")
